10a8b235c0f88c1de592ac0a199adb73f64ac7c3
KukovecRok <rok.kukovec1@student.um.si>
2023-02-02 15:04:56 +0100

    Bump version to 0.2.3

--- tests/test_thumbnails_readme.py
+++ tests/test_thumbnails_readme.py
@@ -27,7 +27,7 @@ path_to_readme = Path(path + "/___README.md")
 
 
 def test_version():
-    assert __version__ == "0.2.2"
+    assert __version__ == "0.2.3"
 
 
 def test_thumbnails_folder_creation():
